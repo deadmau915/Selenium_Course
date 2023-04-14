900519591bd033c430e88a9b814ef97d0341fdb8
Mauricio Marin <mauriciomarin915@gmail.com>
2023-04-13 22:24:17 -0500

    added some functionalities, like filter by condition, also an assertion about have selected the filter new

--- mercadolibre_single_test.py
+++ mercadolibre_single_test.py
@@ -4,14 +4,16 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 from pyunitreport import HTMLTestRunner
 
 class MercadoLibreTest(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver = self.driver
+    @classmethod
+    def setUp(cls):
+        cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver = cls.driver
         driver.implicitly_wait(10)
         driver.maximize_window()
         driver.get("https://mercadolibre.com/")
@@ -21,12 +23,30 @@ class MercadoLibreTest(unittest.TestCase):
         country = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "CO")))
         country.click()
 
-        search_bar = driver.find_element(By.ID, "cb1-edit")
+        try:
+            WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '(//button[contains(@class, "cookie-consent-banner")])[1]'))).click()
+        except:
+            print("You already accepted the cookies in this website")
+
+        search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "cb1-edit")))
         search_bar.send_keys("Playstation 4")
         search_bar.submit()
+
+        filter_condition_new = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//a[@aria-label="Nuevo"]')))
+        filter_condition_new.click()
+
+        self.assertTrue(self.is_element_present(By.TAG_NAME, '//div[@class="andes-tag__label"]'))
     
-    def tearDown(self):
-        self.driver.close()
+    @classmethod
+    def tearDown(cls):
+        cls.driver.close()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by = how, value = what)
+        except NoSuchElementException as variable:
+            return False
+        return True
     
 if __name__=="__main__":
     unittest.main(verbosity=2, testRunner=HTMLTestRunner(output="mercadolibre_singletest"))
